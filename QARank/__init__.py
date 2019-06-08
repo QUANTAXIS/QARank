6ed951b968c706bb4c8328d24ca587217e618799
yutiansut <yutiansut@qq.com>
2019-06-08 15:51:22 +0800

    #update init

--- QARank/__init__.py
+++ QARank/__init__.py
@@ -1,5 +1,20 @@
 from QUANTAXIS.QARP.QARisk import QA_Risk, QA_Performance
-
+from QUANTAXIS.QAARP.QAAccount import QA_Account
 
 class QA_Rank():
-    def __init__(acc:QA_Account)
+    def __init__(acc:QA_Account):
+        self.acc = acc
+
+        self.risk = QA_Risk(acc)
+        self.performance = QA_Performance(acc)
+        
+        print(acc)
+
+    def rank(self):
+        pass
+
+    @property
+    def message(self):
+        return {
+            'alpha': self.risk.alpha
+        }
